7a18b803d88a9e3169fa161da8038c5a61be58eb
MyguelCode <miguelsierracpi@gmail.com>
2025-11-13 22:21:14 -0500

    Delete js directory

--- js/features/formatting/Linkifier.js
+++
@@ -1,9 +0,0 @@
-// === LINKIFIER - UNINOTE ===
-
-export class Linkifier {
-  static linkify(element) {
-    // Código actual en legacy líneas 1235-1271
-    // Convierte URLs en links clickables
-    console.log('Linkifier: Usando versión legacy');
-  }
-}
